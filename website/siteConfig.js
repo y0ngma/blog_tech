79a98c73af06ceaf7e909a347b3058eb4b0a7856
y0ngma <takeit2sy@pusan.ac.kr>
2020-12-20 23:50:45 +0900

    개인 깃 정보 기입

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,18 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'Quantum AI tech blog',// Title for your website.
+  tagline: '폰트좀 바꿔야할듯',
+  
+  url: 'https://y0ngma.github.io', // Your website URL
+  baseUrl: '/blog_tech/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'blog_tech',
+  organizationName: 'y0ngma',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
